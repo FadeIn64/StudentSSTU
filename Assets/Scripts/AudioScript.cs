5dc06a5044376ef49c22eed683f2522fb45446dd
ToporkovaNastya <133683009+ToporkovaNastya@users.noreply.github.com>
2023-06-30 12:24:04 +0400

    AudioScript

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource AudioSource;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Computer")
+            AudioSource.Play();
+        print("Collision detected");
+    }
+}
