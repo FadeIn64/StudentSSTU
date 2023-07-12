66d918b4f8da2ed1f002c2a78be0985cbd4e7910
FadeIn64 <danik6052@gmail.com>
2023-07-12 10:28:48 +0400

    Test VFX

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class TestScript : MonoBehaviour
 {
+    private GameObject _particleSystem;
+    
     // Start is called before the first frame update
     void Start()
     {
