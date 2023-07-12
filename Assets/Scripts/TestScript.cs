dbda0ee54dea40204ebba0b79ac97ef24d156235
FadeIn64 <danik6052@gmail.com>
2023-07-12 11:00:27 +0400

    Add Sparks on Scene

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,8 +7,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TestScript : MonoBehaviour
 {
-    private GameObject _particleSystem;
-    
+
     // Start is called before the first frame update
     void Start()
     {
